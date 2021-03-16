b8d64c9bfa304890e726bb54e355c551494eef99
Victor Malod <mcreaper@hotmail.fr>
2021-03-16 15:56:26 +0100

    finished the parser side

--- src/core/parser.rs
+++ src/core/parser.rs
@@ -1,48 +1,53 @@
 extern crate pest;
 
 use pest::Parser;
+use pest::error::Error;
+use pest::iterators::Pairs;
 
 #[derive(Parser)]
 #[grammar = "core/parallel.pest"]
 pub struct ParallelParser;
 
-use std::fs;
-
-pub fn parse() {
-    //dirty error handling here, just testing.
-    let unparsed_file = fs::read_to_string("inputexample.txt").expect("cannot read file");
-    
-    let inputs = ParallelParser::parse(Rule::main, &unparsed_file)
-    .expect("unsuccessful parse") // unwrap the parse result
-    .next().unwrap(); // get and unwrap the `main` rule; never fails
-
-    for token in inputs.into_inner() {
-        match token.as_rule() {
-            // some rules are not reachable from main rule, 
-            // that is totaly normal according to the grammar.
-            Rule::options => println!("option : {}", token.as_str()),
-            Rule::commands => {
-                println!("command : {}", token.as_str());
-                for arg in token.into_inner().skip(1) /*skip the first item*/ {
-                    print!("argument : {} (of type", arg.as_str());
-                    let rule = arg.into_inner().next().unwrap().as_rule();
-                    match rule {
-                        Rule::target => println!(" target)"),
-                        Rule::quoted_char => println!(" quoted char)"),
-                        Rule::string => println!(" string)"),
-                        _ => unreachable!(),
-                    }
-                }
-            }
-            Rule::separators => {
-                println!("separator : {}", token.as_str());
-                for input in token.into_inner().skip(1) {
-                    println!("input : {}", input.as_str());
-                }
-            }
-            _ => unreachable!(),
+pub fn parse(raw_string: &String) -> Result<Pairs<Rule>, Error<Rule>> {   
+    let parse_result = ParallelParser::parse(Rule::main, &raw_string);
+    let inputs = match parse_result {
+        Ok(pairs) => pairs,
+        Err(error) => {
+            eprintln!("The arguments entered are incorrect.");
+            return Err(error);
         }
-    }
+    };
+    return Ok(inputs);
 
-    println!("fin parse");
+    // let inputs = inputs.next().unwrap(); // get and unwrap the `main` rule; never fails
+
+    // for pairs in inputs.into_inner() {
+    //     match pairs.as_rule() {
+    //         Rule::options => println!("option : {}", pairs.as_str()),
+    //         Rule::commands => {
+    //             println!("command : {}", pairs.as_str());
+    //             for arg in pairs.into_inner().skip(1) /*skip the first item*/ {
+    //                 print!("argument : {} (of type", arg.as_str());
+    //                 let rule = arg.into_inner().next().unwrap().as_rule();
+    //                 match rule {
+    //                     Rule::target => println!(" target)"),
+    //                     Rule::quoted_char => println!(" quoted char)"),
+    //                     Rule::string => println!(" string)"),
+    //                     _ => unreachable!(),
+    //                 }
+    //             }
+    //         }
+    //         Rule::separators => {
+    //             println!("separator : {}", pairs.as_str());
+    //             for input in pairs.into_inner().skip(1) {
+    //                 println!("input : {}", input.as_str());
+    //             }
+    //         }
+    //         // some rules are not reachable from main rule, 
+    //         // that is totaly normal according to the grammar.
+    //         _ => unreachable!(),
+    //     }
+    // }
+
+    // return Ok(inputs);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,7 @@ pub mod core;
 pub mod parallel;
 pub mod remote;
 
-// an `extern crate` loading macros must be at the crate root
-#[macro_use] extern crate pest_derive;
+// an 'extern crate' loading macros must be at the crate root
+// according to rust rules because macros are handled early 
+// enough in the compilation stage that order matters.
+#[macro_use] extern crate pest_derive; // for parsing with pest
