32d47f3b0ba4d89ff1e896580d8982ccfe012dc9
Dorian Baret <bdepolytechgre@gmail.com>
2021-03-29 11:53:52 +0200

    merge : resolve warning

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use rust_parallel::parallel::Parallel;
 use std::env;
-use rust_parallel::core::jobmanager::JobManager;
-use rust_parallel::core::interpretor;
 
 fn main() {
     #[cfg(debug_assertions)]
