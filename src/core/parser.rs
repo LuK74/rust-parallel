f8975b34ddf7fc9ce5019f1b22d8f450c7226c92
Victor Malod <mcreaper@hotmail.fr>
2021-03-29 10:37:19 +0200

    pest: remove some unecessary comments

--- src/core/parser.rs
+++ src/core/parser.rs
@@ -18,36 +18,4 @@ pub fn parse(raw_string: &String) -> Result<Pairs<Rule>, Error<Rule>> {
         }
     };
     return Ok(inputs);
-
-    // let inputs = inputs.next().unwrap(); // get and unwrap the `main` rule; never fails
-
-    // for pairs in inputs.into_inner() {
-    //     match pairs.as_rule() {
-    //         Rule::options => println!("option : {}", pairs.as_str()),
-    //         Rule::commands => {
-    //             println!("command : {}", pairs.as_str());
-    //             for arg in pairs.into_inner().skip(1) /*skip the first item*/ {
-    //                 print!("argument : {} (of type", arg.as_str());
-    //                 let rule = arg.into_inner().next().unwrap().as_rule();
-    //                 match rule {
-    //                     Rule::target => println!(" target)"),
-    //                     Rule::quoted_char => println!(" quoted char)"),
-    //                     Rule::string => println!(" string)"),
-    //                     _ => unreachable!(),
-    //                 }
-    //             }
-    //         }
-    //         Rule::separators => {
-    //             println!("separator : {}", pairs.as_str());
-    //             for input in pairs.into_inner().skip(1) {
-    //                 println!("input : {}", input.as_str());
-    //             }
-    //         }
-    //         // some rules are not reachable from main rule, 
-    //         // that is totaly normal according to the grammar.
-    //         _ => unreachable!(),
-    //     }
-    // }
-
-    // return Ok(inputs);
 }
